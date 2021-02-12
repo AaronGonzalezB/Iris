c72d69d40aa258f7255e4bed29796389f5934246
Aarón Al Rachid González Benaissa <45605769+AaronGonzalezB@users.noreply.github.com>
2021-02-11 22:44:40 -0500

    official version

--- iris_petalo.py
+++ iris_petalo.py
@@ -1,8 +1,6 @@
 """
-CLASIFICADOR FLORES TIPO IRIS CON IRIS DATASET A PARTIR DEL PETALO
+Classifying Iris species only with Petal features
 """
-
-# OBTENIENDO LOS DATOS
 
 import numpy as np
 import pandas as pd
@@ -13,15 +11,10 @@ from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 
-# Importamos los datos
+# Import and delete Sepal features
 iris = pd.read_csv("Iris.csv")
-
-# Eliminamos la columna Id ya que pandas crea automaticamente el indice en los dataframes
-# Adicionalmente eliminamos las columnas de sepalo para predecir los valores solo a partir de los petalos
 iris = iris.drop(['Id','SepalLengthCm','SepalWidthCm'], axis = 1)
 print(iris.head())
-
-# ANALIZANDO LOS DATOS
 
 print('Informacion del dataset:')
 print(iris.info())
@@ -32,7 +25,7 @@ print(iris.describe())
 print('Distribucion de datos por especie:')
 print(iris.groupby('Species').size())
 
-# Grafica del Petalo - Longitud vs Ancho
+# Visualize Petal features
 fig = iris[iris.Species == 'Iris-setosa'].plot(kind = 'scatter',
             x = 'PetalLengthCm', y = 'PetalWidthCm', color = 'blue', label = 'Setosa')
 iris[iris.Species == 'Iris-versicolor'].plot(kind = 'scatter',
@@ -45,35 +38,31 @@ fig.set_ylabel('Petalo - Ancho')
 fig.set_title('Petalo - Longitud vs Ancho')
 plt.show()
 
-# APLICACION DE LOS ALGORITMOS DE ML
-
-# Separo las features y el target
+# Target and features
 X = np.array(iris.drop(['Species'],1))
 y = np.array(iris['Species'])
 
-# Separo los datos de entrenamiento y prueba
+# Train/Test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
 print('Son {} datos para entrenamiento y {} datos para prueba'.format(X_train.shape[0], X_test.shape[0]))
 
-# 1. Modelo de Regresion Logistica
+# ML Algorithms
+
 algoritmo = LogisticRegression()
 algoritmo.fit(X_train, y_train)
 Y_pred = algoritmo.predict(X_test)
 print('Precision Regresion Logistica: {}'.format(algoritmo.score(X_train, y_train)))
 
-# 2. Maquinas de Soporte Vectorial SVM
 algoritmo = SVC()
 algoritmo.fit(X_train, y_train)
 Y_pred = algoritmo.predict(X_test)
 print('Precision Maquinas de Soporte Vectorial: {}'.format(algoritmo.score(X_train, y_train)))
 
-# 3. K Vecinos mas cercanos KNN
 algoritmo = KNeighborsClassifier(n_neighbors = 5)
 algoritmo.fit(X_train, y_train)
 Y_pred = algoritmo.predict(X_test)
 print('Precision de K Vecinos mas cercanos: {}'.format(algoritmo.score(X_train, y_train)))
 
-# 4. Arbol de decision
 algoritmo = DecisionTreeClassifier()
 algoritmo.fit(X_train, y_train)
 Y_pred = algoritmo.predict(X_test)
